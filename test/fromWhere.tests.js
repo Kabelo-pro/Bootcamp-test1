b7e4ff43e3b5961867d6d09b8fc9271b41c2bf87
Kabelo Madimabe <madimabekabelo@gmail.com>
2023-04-13 16:29:10 +0200

    Adding suffecient tests to my function

--- test/fromWhere.tests.js
+++ test/fromWhere.tests.js
@@ -1,16 +1,16 @@
 describe('Test my fromWhere function' , function(){
     it("Write a function that takes a car registration number as a parameter and returns the town the car is from.", function(){
-        assert.equal("Bellville", fromWhere("CY"), "this should not be true");
+        assert.equal("Bellville", fromWhere("CY"));
 
         
     });
     it("Write a function that takes a car registration number as a parameter and returns the town the car is from.", function(){
-        assert.equal("Paarl", fromWhere("CJ"), "this should not be true");
+        assert.equal("Paarl", fromWhere("CJ"));
 
         
     });
     it("Write a function that takes a car registration number as a parameter and returns the town the car is from.", function(){
-        assert.equal('Cape Town', fromWhere("CA"), "this should not be true");        
+        assert.equal('Cape Town', fromWhere("CA"));        
     });
 
 });
