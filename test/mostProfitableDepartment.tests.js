e46005d38a7200f2f8141c717d611014b3c73397
Kabelo Madimabe <madimabekabelo@gmail.com>
2023-04-13 16:50:22 +0200

    All functions with multiple unit tests

--- test/mostProfitableDepartment.tests.js
+++ test/mostProfitableDepartment.tests.js
@@ -16,9 +16,56 @@ var salesData = [
     {department : 'carpentry', sales : 16109, day : 'Friday'},
 ];
 
+var salesData1 = [
+    {department : 'hardware', sales : 4500, day : 'Monday'},
+    {department : 'garden', sales : 1500, day : 'Monday'},
+    {department : 'carpentry', sales : 5500, day : 'Monday'},
+    {department : 'hardware', sales : 7500, day : 'Tuesday'},
+    {department : 'garden', sales : 2505, day : 'Tuesday'},
+    {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+    {department : 'hardware', sales : 1500, day : 'Wednesday'},
+    {department : 'garden', sales : 8507, day : 'Wednesday'},
+    {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+    {department : 'hardware', sales : 12000, day : 'Thursday'},
+    {department : 'garden', sales : 18007, day : 'Thursday'},
+    {department : 'carpentry', sales : 6109, day : 'Thursday'},
+    {department : 'hardware', sales : 7005, day : 'Friday'},
+    {department : 'garden', sales : 12006, day : 'Friday'},
+    {department : 'carpentry', sales : 16109, day : 'Friday'},
+];
+
+var salesData2 = [
+    {department : 'hardware', sales : 4500, day : 'Monday'},
+    {department : 'indoor', sales : 1500, day : 'Monday'},
+    {department : 'carpentry', sales : 5500, day : 'Monday'},
+    {department : 'hardware', sales : 7500, day : 'Tuesday'},
+    {department : 'indoor', sales : 2505, day : 'Tuesday'},
+    {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+    {department : 'hardware', sales : 1500, day : 'Wednesday'},
+    {department : 'indoor', sales : 8507, day : 'Wednesday'},
+    {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+    {department : 'hardware', sales : 12000, day : 'Thursday'},
+    {department : 'indoor', sales : 18007, day : 'Thursday'},
+    {department : 'carpentry', sales : 6109, day : 'Thursday'},
+    {department : 'hardware', sales : 7005, day : 'Friday'},
+    {department : 'indoor', sales : 12006, day : 'Friday'},
+    {department : 'carpentry', sales : 16109, day : 'Friday'},
+];
 describe('Test my mostProfitableDepartment function', function () {
     it("Write a function to determine which of their departments are the most profitable", function () {
         assert.deepEqual("outdoor", mostProfitableDepartment(salesData));
+
+
+    });
+
+    it("Write a function to determine which of their departments are the most profitable", function () {
+        assert.deepEqual("garden", mostProfitableDepartment(salesData1));
+
+
+    });
+
+    it("Write a function to determine which of their departments are the most profitable", function () {
+        assert.deepEqual("indoor", mostProfitableDepartment(salesData2));
 
 
     });
